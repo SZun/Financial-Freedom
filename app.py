ce4da024ae49cf2338a098b8a967b4609dde5595
SZun <zunsamuel97@gmail.com>
2023-02-14 18:57:25 -0600

    using streamlit form

--- app.py
+++ app.py
@@ -3,63 +3,52 @@ from calculator import Calculator
 
 st.write("# Next Dollar")
 
-st.write("## Your Financials")
+with st.form("Your Financials"):
+    st.write("## Your Financials")
 
-st.write("### 401k")
-match_percentage_401k = st.text_input("Employer Match %")
-st.write("#### Portfolio Mix")
-st.write("##### Stocks")
-stocks_401k = st.text_input("401k Stock %")
-st.write("##### Bonds")
-bonds_401k = st.text_input("401k Bonds %")
-
-
-st.write("### IRA")
-expected_ira_return = st.text_input("Expected Yearly Return")
-st.write("#### Portfolio Mix")
-st.write("##### Stocks")
-stocks_ira = st.text_input("IRA Stock %")
-st.write("##### Bonds")
-bonds_ira = st.text_input("IRA Bonds %")
-
-st.write("### Credit Cards")
-st.write("##### Primary Card")
-primary_card_interest_rate = st.text_input("Primary Interest Rate")
-primary_card_debt = st.text_input("Total Primary Debt")
-st.write("##### Secondary Card")
-secondary_card_interest_rate = st.text_input("Secondary Interest Rate")
-secondary_card_debt = st.text_input("Total Secondary Debt")
+    st.write("### 401k")
+    match_percentage_401k = st.text_input("Employer Match %")
+    st.write("#### Portfolio Mix")
+    st.write("##### Stocks")
+    stocks_401k = st.text_input("401k Stock %")
+    st.write("##### Bonds")
+    bonds_401k = st.text_input("401k Bonds %")
 
 
+    st.write("### IRA")
+    expected_ira_return = st.text_input("Expected Yearly Return")
+    st.write("#### Portfolio Mix")
+    st.write("##### Stocks")
+    stocks_ira = st.text_input("IRA Stock %")
+    st.write("##### Bonds")
+    bonds_ira = st.text_input("IRA Bonds %")
 
+    st.write("### Credit Cards")
+    st.write("##### Primary Card")
+    primary_card_interest_rate = st.text_input("Primary Interest Rate")
+    primary_card_debt = st.text_input("Total Primary Debt")
+    st.write("##### Secondary Card")
+    secondary_card_interest_rate = st.text_input("Secondary Interest Rate")
+    secondary_card_debt = st.text_input("Total Secondary Debt")
 
+    if st.form_submit_button("Submit"):
 
-def reccomend(calculator):
-        reccomendation = calculator.generate_reccommendation()
-    
-        st.write("## Your Reccomendation")
-        st.write(reccomendation)
+        try:
+            calculator = Calculator(
+            float(match_percentage_401k),
+            float(stocks_401k),
+            float(bonds_401k),
+            float(expected_ira_return),
+            float(stocks_ira),
+            float(bonds_ira),
+            float(primary_card_interest_rate),
+            float(primary_card_debt),
+            float(secondary_card_interest_rate),
+            float(secondary_card_debt)
+        )
 
-
-
-
-
-if st.button("Submit"):
-    try:
-        calculator = Calculator(
-        float(match_percentage_401k),
-        float(stocks_401k),
-        float(bonds_401k),
-        float(expected_ira_return),
-        float(stocks_ira),
-        float(bonds_ira),
-        float(primary_card_interest_rate),
-        float(primary_card_debt),
-        float(secondary_card_interest_rate),
-        float(secondary_card_debt)
-    )
-
-        reccomend(calculator)
-    except:
-        st.error('Invalid Input', icon="🚨")
+            st.write("## Your Reccomendation")
+            st.write(calculator.generate_reccommendation())
+        except:
+            st.error('Invalid Input', icon="🚨")
 
